2d9936f3246ce269f8d02be5e32c96fb9a1c4208
djsweet04 <djsweet@udel.edu>
2025-09-28 14:25:06 -0400

    Potential fix to prisma, created base level course backend

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { LinksModule } from './links/links.module';
-
+import { PrismaService } from './prisma.service';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { StudentsModule } from './students/students.module';
@@ -9,6 +9,7 @@ import { StudentsModule } from './students/students.module';
 @Module({
   imports: [LinksModule, StudentsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
