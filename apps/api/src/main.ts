fe14eb331d62e3da8d9289e56473186613b7b223
djsweet04 <djsweet@udel.edu>
2025-10-10 13:18:43 -0400

    Fixed CORS issue

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,16 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '../../.env' });
+import * as cors from 'cors';
 //import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || undefined;
-
+  console.log('CORS origin set to:', process.env.FRONTEND_URL);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
-    methods: ['GET,POST,PUT,DELETE'],
+    //origin: '*',
+    methods: ['GET,POST,PUT,DELETE,OPTIONS'],
     credentials: true,
   });
   await app.listen(port, host);
